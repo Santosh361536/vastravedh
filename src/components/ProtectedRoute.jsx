0f0035005f85ba24651de212e934cbeb3c6179c8
tadi sai santosh <o210943@rguktong.ac.in>
2025-11-08 17:00:03 +0530

    Delete src directory

--- src/components/ProtectedRoute.jsx
+++
@@ -1,8 +0,0 @@
-import { useAuth } from '@/contexts/AuthContext';
-import { Navigate } from 'react-router-dom';
-
-export default function ProtectedRoute({ children }) {
-  const { user, loading } = useAuth();
-  if (loading) return null; // or show a spinner
-  return user ? children : <Navigate to="/signin" replace />;
-}
